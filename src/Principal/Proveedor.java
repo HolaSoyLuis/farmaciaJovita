bc4fa2d43969a2386d18f61edad1084a584403e6
werner <werner@DESKTOP-J3TBM0R.lan>
2016-11-11 13:06:08 -0600

    reportes 2

---
+++ src/Principal/Proveedor.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author werner
+ */
+public class Proveedor extends Usuario {
+    
+//    public void ingresarProveedor(String sql,String Nombre, String Telefono,String tipo){
+//      
+//        
+//        try{
+//            pst = conn.prepareStatement(sql);
+//            pst.setString(1, user);
+//            pst.setString(2, pass);
+//            pst.setString(3, tipo);
+//            rs = pst.executeQuery();
+//            JOptionPane.showMessageDialog(null, "Saved");
+//        pst.close();
+//        rs.close();
+//        }catch(Exception e){
+//            System.out.println("Error "+e);
+//        }
+//       
+//    }//Finaliza ingresar usuario
+    
+}//Finaliza clase proveedor
